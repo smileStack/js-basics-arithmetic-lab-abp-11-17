a64ed2f903936a74de2e80add7cda438e480c728
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-11-13 20:52:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
-let newID = oldID + 1000000000;
+const newID = oldID + 1000000000;
 const ageIsValid;
